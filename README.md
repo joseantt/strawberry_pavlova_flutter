5b2f302434898da2a58a12aa88275c4c1bb73186
joseantt <joseant994@gmail.com>
2024-10-22 08:53:56 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_strawberry_pavlova
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_strawberry_pavlova/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_strawberry_pavlova
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
